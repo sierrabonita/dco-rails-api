2191908e201f684c38f80a94ec251a0a10b8ca16
sierrabonita <workbj1212@gmail.com>
2026-05-17 10:17:11 +0900

    Fix formatting in routes.rb for consistency

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
   resources :skills
   resources :users do
-    resources :posts, only: [ :index ]
+    resources :posts, only: [:index]
   end
 
   resources :posts
 
-
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get "up", to: "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
   # root "posts#index"
