9b7ef6da503da0e86ceb508b930ba795771f095b
sierrabonita <workbj1212@gmail.com>
2026-05-18 11:08:55 +0900

    fix: ユーザー初期化スクリプトのパスワードを修正

--- db/seeds/users.rb
+++ db/seeds/users.rb
@@ -2,7 +2,7 @@ puts "=== Usersの初期化を開始します ==="
 
 User.destroy_all
 
-users = [{ name: "山田太郎", email: "test_1@test.com", password: "P@ssword" }]
+users = [{ name: "山田太郎", email: "test_1@test.com", password: "P@ssword1" }]
 
 users.each { |user_param| User.create!(user_param) }
 
