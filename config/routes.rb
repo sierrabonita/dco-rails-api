4aac5614f1f8311016c01d7927dfabaf979a0ba8
sierrabonita <workbj1212@gmail.com>
2026-05-23 06:14:35 +0900

    feat: postsリソースを削除

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,6 @@ Rails.application.routes.draw do
     resources :posts, only: [:index]
   end
 
-  resources :posts
-
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up", to: "rails/health#show", as: :rails_health_check
