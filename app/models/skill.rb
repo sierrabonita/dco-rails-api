3d7e433aef58b6d1e7fb2a7ee56975265e6d10b9
sierrabonita <workbj1212@gmail.com>
2026-06-29 10:45:13 +0900

    refactor: Skill にコメント追加

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# スキルのマスターデータを管理し、ユーザーとの関連付けや検証を行うモデル
 class Skill < ApplicationRecord
   has_many :user_skills, dependent: :destroy
   has_many :users, through: :user_skills
